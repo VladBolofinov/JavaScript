a27427e0c05b48a417b2979b2a9bec9794828562
VladBolofinov <vlad.bolofinov@gmail.com>
2022-06-06 19:31:18 +0300

    learn_js

---
+++ Test_lesson1/script.js
@@ -0,0 +1,88 @@
+"use strict";
+
+/* let number = 5;
+const LeftBorderWidth = 1;
+
+number = 10;
+console.log(number); */
+
+/* const obj = {
+    a: 50
+};
+
+obj.a =10; */
+
+/* console.log(obj); */
+
+/* console.log(name);
+var name = 'Ivan';
+
+{
+    var result = 50;
+}
+
+console.log(result); */
+
+/* let number = 4.6; */
+/* 
+console.log(4/0);
+console.log('string' * 9);
+
+const person = 'asdawda';
+
+const bool = false; */
+
+/* console.log(something);
+
+let und;
+console.log(und);
+
+const obj = {
+    name: 'John',
+    age: 25,
+    isMarried: false
+}; */
+/* 
+console.log(obj.name);
+
+let arr = ['plum.png', 'keks.jpg', 6, 'John'];
+console.log(arr[2]);
+
+const arr = ['a', 'b', 'c'];
+const arrObj = {
+    a: 'a',
+    '1': 'b',
+    2: 'c'
+} */
+
+/* arrObj.b = '1234'; */
+/* arrObj['b'] = '1234'; */
+
+/* console.log(arrObj['b']);
+console.log(arrObj.b); */
+
+/*  */
+
+
+/* const result = confirm("Are you here?");
+console.log(result);
+
+const answer = +prompt("Вам есть 18?", "18");
+console.log(answer + 5);
+
+const answers = [];
+answers[0] = promt('Как ваше имя?' '');
+answers[1] = promt('Какова ваша фамилия?' '');
+answers[2] = promt('Сколько вам лет?' '');
+
+document.write(answers); */
+
+/* интерполяция */
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user = 'Ivan';
+
+alert(`Привет, ${user}`);
