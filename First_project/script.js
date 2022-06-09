1ab700cfbb86b40dc44b7684836372fc76d69971
Vlad_Bolofinov <vlad.bolofinov@gmail.com>
2022-06-09 14:36:11 +0300

    done_full_task

--- First_project/script.js
+++ First_project/script.js
@@ -1,4 +1,6 @@
 "use strict";
+                                 
+/* Моё решение */
 
 const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
 
@@ -23,3 +25,19 @@ const personalMovieDB = {
 
 console.log(personalMovieDB.movies);
 
+
+/* Решение другим способом */
+
+/* const personalMovieDB = {
+    movies: {}
+}
+
+const a = prompt("Один из последних просмотренных фильмов?", "",),
+      b = prompt("На сколько оцените его?",""),
+      c = prompt("Один из последних просмотренных фильмов?", "",),
+      d = prompt("На сколько оцените его?","");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB); */
