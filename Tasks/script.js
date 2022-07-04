25c425aada5cc60335ff30fb1699140f7b2d1170
Vlad_Bolofinov <vlad.bolofinov@gmail.com>
2022-07-05 01:13:19 +0300

    doneFourthTask

--- Tasks/script.js
+++ Tasks/script.js
@@ -123,5 +123,69 @@ function getTimeFromMinutes(minutes) {
 getTimeFromMinutes(60);
 
 
+/* Другое решение
+function getTimeFromMinutes(minutesTotal) {
+    if (typeof(minutesTotal) !== 'number' || minutesTotal < 0 || !Number.isInteger(minutesTotal)) {
+        return "Ошибка, проверьте данные";
+    }
+
+    const hours = Math.floor(minutesTotal / 60);
+    const minutes = minutesTotal % 60;
+
+    let hoursStr = '';
+
+    switch (hours) {
+        case 0: 
+            hoursStr = 'часов';
+            break;
+        case 1:
+            hoursStr = 'час';
+            break;
+        case 2:
+        case 3:
+        case 4:
+            hoursStr = 'часа';
+            break;
+        default:
+            hoursStr = 'часов';
+    }
+
+    return `Это ${hours} ${hoursStr} и ${minutes} минут`;
+}
+
+getTimeFromMinutes(180) */
+
+//Задача 4. Написать функцию которая принимает в себя 4 аргумента и возвращает наибольшее число 
+// из заданных аргументов. Если попадает не число или аргументов меньше 4, то возвращает 0.
+//Моё решение
+
+function findMaxNumber(a, b, c, d) {
+    if (arguments.length < 4 || typeof(arguments[0, 1, 2, 3]) !== 'number') {
+        return 0;
+    } else {
+       return Math.max(a,b,c,d);
+    } 
+}
+
+findMaxNumber(85, 69, 74, 198);
+
+/* Другое решение
+function findMaxNumber(a, b ,c, d) {
+    // Самое простое - это использовать Math.max :)
+    // А оптимизировать такую проверку мы научимся совсем скоро
+    if (typeof(a) !== 'number' ||
+        typeof(b) !== 'number' ||
+        typeof(c) !== 'number' ||
+        typeof(d) !== 'number') {
+        return 0;
+    } else {
+        return Math.max(a, b ,c, d);
+    }
+}
+
+findMaxNumber(1, 5, 6.6, 10.5);
+findMaxNumber(1, 5, '6', '10'); */
+
+
 
 
