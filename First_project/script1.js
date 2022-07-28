93c48f53e0e56a3dc1d63f69455657712af86509
Vlad_Bolofinov <vlad.bolofinov@gmail.com>
2022-07-28 18:41:25 +0300

    doneTasksProject

---
+++ First_project/script1.js
@@ -0,0 +1,47 @@
+"use strict";
+
+ /* Практика ч.1 */              
+/* Моё решение */
+
+/* const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const firstLastMovie = prompt("Один из последних просмотренных фильмов?", "",);
+
+const firstScore = prompt("На сколько оцените его?","");
+
+const secondLastMovie = prompt("Один из последних просмотренных фильмов?", "",);
+
+const secondScore = prompt("На сколько оцените его?","");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {
+        [firstLastMovie]: firstScore,
+        [secondLastMovie]: secondScore,
+    },
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+console.log(personalMovieDB.movies);
+
+
+/* Решение другим способом */
+
+/* const personalMovieDB = {
+    movies: {}
+}
+
+const a = prompt("Один из последних просмотренных фильмов?", "",),
+      b = prompt("На сколько оцените его?",""),
+      c = prompt("Один из последних просмотренных фильмов?", "",),
+      d = prompt("На сколько оцените его?","");
+
+personalMovieDB.movies[a] = b;    ←←← (синтаксис: переменная а добавляется в объект movies 
+                                        т.к. до этого не была создана)
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB); */
+
+
--- task_arrays/script2.js
+++ task_arrays/script2.js
@@ -27,18 +27,16 @@ reverse(someString);
 const baseCurrencies = ['USD', 'EUR'];
 const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
 const allCurrencies = baseCurrencies.concat(additionalCurrencies);
-const lostCurrencies = 'CNY';
-let str = 'Доступные валюты:';
-
 
 function availableCurr(arr, missingCurr) {
+    let str = 'Доступные валюты:';
     let i = 0;
     if (arr.length <= 0) {
         console.log(`Нет доступных валют`);
     }
     for (let value of arr) {
         if (value !== missingCurr) {
-            str += `     \n  ${arr[i]}`;
+            str += `\n  ${arr[i]}`;
             i++;
         } else {
             i++;
@@ -46,4 +44,54 @@ function availableCurr(arr, missingCurr) {
     }
     console.log(str);
 }
-availableCurr(allCurrencies, lostCurrencies);
+availableCurr(allCurrencies, 'EUR');
+
+//Ниже другие паттерны решения:
+/* 
+const someString = 'This is some strange string';
+
+function reverse(str) {
+    if (typeof(str) !== 'string') {
+        return "Ошибка!";
+    }
+    // Самый оптимальный вариант решения
+    return str.split('').reverse().join('');
+
+    // Решение при помощи цикла
+    // let newStr = '';
+    // for (let i = str.length - 1; i >= 0; i--) {
+    //     newStr += str[i];
+    // }
+    // return newStr
+}
+
+reverse(someString);
+
+const baseCurrencies = ['USD', 'EUR'];
+const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
+
+function availableCurr(arr, missingCurr) {
+    let str = '';
+    arr.length === 0 ? str = 'Нет доступных валют' : str = 'Доступные валюты:\n';
+
+    arr.forEach(function(curr, i) {
+        if (curr !== missingCurr) {
+            str += `${curr}\n`;
+        }
+    });
+
+    // Или
+    // for (let i = 0; i < arr.length; i++) {
+    //     if (arr[i] === missingCurr) {
+    //         continue;
+    //     }
+    //     str += `${arr[i]}\n`;
+    // }
+
+    return str;
+}
+
+availableCurr([...baseCurrencies, ...additionalCurrencies], 'CNY')
+
+
+*/
